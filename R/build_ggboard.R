c9f5e2a74972c30cf09e38a34504fcee76f79a40
Alex Kalinka <alex.kalinka@cancer.org.uk>
2021-12-31 21:31:29 +0100

    'build_ggboard' first pass.

--- R/build_ggboard.R
+++ R/build_ggboard.R
@@ -2,11 +2,15 @@
 #' 
 #' Creates a `ggplot2` object of the Game of Life board for a data frame of 1 or more generations.
 #' 
-#' @param board_df A data frame containing the co-ordinates of living cells, as produced by `gameRlife::board_df`.
+#' @param board_DF A data frame containing the co-ordinates of living cells, as produced by `gameRlife::board_df`.
 #' 
 #' @return An object of class `ggplot`.
 #' @author Alex Kalinka, \email{alex.t.kalinka@@gmail.com}
+#' @importFrom dplyr %>%
+#' @importFrom ggplot2 ggplot aes geom_tile
 #' @export
-build_ggboard <- function(board_df){
+build_ggboard <- function(board_DF){
+  pl <- board_DF %>%
+    ggplot2::ggplot(ggplot2::aes(col, row))
   
 }
